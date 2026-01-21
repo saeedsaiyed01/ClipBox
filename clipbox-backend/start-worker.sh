3381e93fab67c7f9a086782ed04da3ff3074a15d
Saeed <149547355+saeedsaiyed01@users.noreply.github.com>
2026-01-21 16:47:24 +0530

    docker changes

--- clipbox-backend/start-worker.sh
+++ clipbox-backend/start-worker.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-# Start the worker process directly
+# Start a simple HTTP server to satisfy Render's port requirement
+# using python (since we switched to the fat node image which has python)
+python3 -m http.server $PORT &
+
+# Start the worker process
 echo "Starting Dedicated Worker Process..."
 npm run worker
